625f9df5340ddc992a6c88a6c04ee14ff5366a8c
bbibbibiq <peerasorn@hot-now.com>
2022-05-19 12:17:56 +0700

    Add .env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import { BACKEND_URL } from './constant';
 
 function App() {
   useEffect(() => {
     const fetching = async () => {
-      const response = await fetch('http://localhost:3001/');
+      const response = await fetch(`${BACKEND_URL}`);
       const result = await response.json();
 
       console.log(result);
