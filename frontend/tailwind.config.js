96ffc63d934cf0446fda35f34c3286b897493898
bbibbibiq <peerasorn@hot-now.com>
2022-05-20 17:13:41 +0700

    Font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['./src/*.tsx'],
+  content: ['./src/*.tsx', './src/**/*.tsx'],
   theme: {
     extend: {},
+    fontFamily: {
+      bebas: ['Bebas Neue', 'cursive'],
+      prompt: ['Prompt', 'sans-serif'],
+    },
   },
   plugins: [],
 };
