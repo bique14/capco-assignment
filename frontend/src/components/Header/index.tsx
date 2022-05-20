2a2f02f110f8de9e09ad1e4a8829ce63efa7e7ee
bbibbibiq <peerasorn@hot-now.com>
2022-05-21 01:17:16 +0700

    header styling

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -20,23 +20,29 @@ const Header = () => {
 
   return (
     <div className="bg-black text-white font-bebas px-6 py-5">
-      <div className="flex justify-between items-center">
-        <div className={classNames(['text-4xl cursor-default', 'md:text-6xl'])}>
-          CAPCO
-        </div>
-        <div className="flex items-center gap-5">
-          <div className={classNames(['flex gap-3', 'md:gap-1'])}>
-            {categories.map(category => (
-              <HeaderItem key={category.id} {...category} />
-            ))}
+      <div
+        className={classNames(['mx-auto', 'md:w-11/12', 'lg:w-4/5 lg:mx-auto'])}
+      >
+        <div className="flex justify-between items-center">
+          <div
+            className={classNames(['text-4xl cursor-default', 'md:text-6xl'])}
+          >
+            CAPCO
           </div>
-          <div className={classNames(['hidden', 'md:block md:w-44'])}>
-            <SearchInput />
+          <div className="flex items-center gap-5">
+            <div className={classNames(['flex gap-3', 'md:gap-1'])}>
+              {categories.map(category => (
+                <HeaderItem key={category.id} {...category} />
+              ))}
+            </div>
+            <div className={classNames(['hidden', 'md:block md:w-44'])}>
+              <SearchInput />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={classNames(['block w-full mt-2', 'md:hidden'])}>
-        <SearchInput />
+        <div className={classNames(['block w-full mt-2', 'md:hidden'])}>
+          <SearchInput />
+        </div>
       </div>
     </div>
   );
