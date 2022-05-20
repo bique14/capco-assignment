dfbcab36f2b4b7c54a93a023b4a4b744956ef0b4
bbibbibiq <peerasorn@hot-now.com>
2022-05-21 00:25:12 +0700

    remove unused

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,50 @@
-import React, { useEffect } from 'react';
-import { BACKEND_URL } from './constant';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import { Header, Footer } from './components';
+import { News, Regions, TV, Videos } from './pages';
+
 import './App.css';
+import { classNames } from './lib';
+
+interface IPage {
+  NEWS: string;
+  REGIONS: string;
+  TV: string;
+  VIDEOS: string;
+}
+
+export const PAGE: IPage = Object.freeze({
+  NEWS: 'news',
+  REGIONS: 'regions',
+  TV: 'tv',
+  VIDEOS: 'videos',
+});
 
 function App() {
-  useEffect(() => {
-    const fetching = async () => {
-      const response = await fetch(`${BACKEND_URL}`);
-      const result = await response.json();
-
-      console.log(result);
-    };
-
-    fetching();
-  }, []);
-
   return (
-    <div className="bg-red-200 text-green-600">!Hello w1orld from Frontend</div>
+    <div className="h-full">
+      <BrowserRouter>
+        <Header />
+        <div
+          className={classNames([
+            'mx-auto mb-20',
+            'md:w-11/12',
+            'lg:w-4/5 lg:mx-auto',
+          ])}
+        >
+          <Routes>
+            <Route path="/" element={<News />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/regions" element={<Regions />} />
+            <Route path="/tv" element={<TV />} />
+            <Route path="/videos" element={<Videos />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
