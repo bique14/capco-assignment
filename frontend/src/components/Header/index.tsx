9312eee32905a6a3fab5fa81f5f5f15729f1d81e
bbibbibiq <peerasorn@hot-now.com>
2022-05-21 00:24:32 +0700

    header styling

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import HeaderItem from './HeaderItem';
+import SearchInput from './SearchInput';
+
+import { PAGE } from '../../App';
+import { classNames } from '../../lib';
+
+interface Category {
+  id: string;
+  name: string;
+}
+
+const Header = () => {
+  const categories: Category[] = [
+    { id: PAGE.NEWS, name: 'News' },
+    { id: PAGE.REGIONS, name: 'Regions' },
+    { id: PAGE.TV, name: 'TV' },
+    { id: PAGE.VIDEOS, name: 'Video' },
+  ];
+
+  return (
+    <div className="bg-black text-white font-bebas px-6 py-5">
+      <div className="flex justify-between items-center">
+        <div className={classNames(['text-4xl cursor-default', 'md:text-6xl'])}>
+          CAPCO
+        </div>
+        <div className="flex items-center gap-5">
+          <div className={classNames(['flex gap-3', 'md:gap-1'])}>
+            {categories.map(category => (
+              <HeaderItem key={category.id} {...category} />
+            ))}
+          </div>
+          <div className={classNames(['hidden', 'md:block md:w-44'])}>
+            <SearchInput />
+          </div>
+        </div>
+      </div>
+      <div className={classNames(['block w-full mt-2', 'md:hidden'])}>
+        <SearchInput />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
